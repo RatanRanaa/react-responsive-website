83b5f1da0000c83eeb81f9841c9e94ee581ebee5
Ratan Rana <ratanrana206@gmail.com>
2023-01-17 10:45:18 +0530

    resize

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
         <div className="banner_content">
             <div className="container">
                 <div className="banner_text">
-                <h3>Pizza Devlivery</h3>
-                <h1>MAESTRO PIZINNI</h1>
+                <h3>Pizza Delivery</h3>
+                <h1>MAESTRO PiZZA</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 <div className="banner_btn">
                     <a href="/" className="btn btn_smart">DEVLERY NOW</a>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,12 +10,12 @@ function Navbar() {
                  <Link to="header" spy={true} smooth={true} offset={-100} duration={500}><img src="/image/logo.png" alt="logo" /></Link>
              </div>
              <ul className='navbar_ul'>
-                 <li><Link to="header" spy={true} smooth={true} offset={-100} duration={500}>Home</Link></li>
-                 <li><Link to="about" spy={true} smooth={true} offset={-50} duration={500}>About</Link></li>
-                 <li><Link to="shop" spy={true} smooth={true} offset={-50} duration={500}>Shop</Link></li>
-                 <li><Link to="menu" spy={true} smooth={true} offset={-50} duration={500}>Menu</Link></li>
-                 <li><Link to="clients" spy={true} smooth={true} offset={-50} duration={500}>Clients</Link></li>
-                 <li><Link to="prices" spy={true} smooth={true} offset={-50} duration={500}>Prices</Link></li>
+                 <li><Link to="header" spy={true} smooth={true} offset={-100} duration={1000}>Home</Link></li>
+                 <li><Link to="about" spy={true} smooth={true} offset={-50} duration={1000}>About</Link></li>
+                 <li><Link to="shop" spy={true} smooth={true} offset={-50} duration={1000}>Shop</Link></li>
+                 <li><Link to="menu" spy={true} smooth={true} offset={-50} duration={1000}>Menu</Link></li>
+                 <li><Link to="clients" spy={true} smooth={true} offset={-50} duration={1000}>Clients</Link></li>
+                 <li><Link to="prices" spy={true} smooth={true} offset={-50} duration={1000}>Prices</Link></li>
              </ul>
          </div>
      </nav>
